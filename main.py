92af2f8b103324754644dd49b3a745eed2bb176e
promastcrate <162557255+promastcrate@users.noreply.github.com>
2024-05-29 13:18:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_got_quote():
+    api_url = 'https://api.gameofthronesquotes.xyz/v1/random'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        quote_data = response.json()
+        character = quote_data.get('character', '')
+        if isinstance(character, str):
+            character_upper = character.upper()
+        else:
+            character_upper = 'Unknown Character'
+        quote = quote_data.get('sentence', '')
+        return character_upper, quote
+    else:
+        return None, None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    character, quote = get_got_quote()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if character is not None and quote is not None:
+        print(f"Персонаж: {character}")
+        print(f'Цитата: "{quote}"')
+    else:
+        print("Ошибка при получении данных. Пожалуйста, проверьте подключение к интернету.")
